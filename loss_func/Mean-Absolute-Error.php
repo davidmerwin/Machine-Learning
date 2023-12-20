f4ce023c5ec9a2d7392fe554fdd6c36f41ecb0ea
Viktor Szépe <viktor@szepe.net>
2023-12-20 21:04:07 +0100

    Follow PSR-12 (#1)
    * Follow PSR-12
    
    * fix
    
    * fix a comment sign

--- loss_func/Mean-Absolute-Error.php
+++ loss_func/Mean-Absolute-Error.php
@@ -1,11 +1,11 @@
 <?php
 
 // Loss Function
-function mbe($y, $y_pred)
+function mae($y, $y_pred)
 {
 
     $c = array_map(function ($a, $b) {
-        return ($a - $b);
+        return abs($a - $b);
     }, $y, $y_pred);
 
     return array_sum($c) / count($c);
@@ -15,4 +15,4 @@ $y1 = array(2,4,6,8,10);
 
 $y2 = array(2,3,5,7,11);
 
-echo mbe($y1, $y2);
+echo mae($y1, $y2);
